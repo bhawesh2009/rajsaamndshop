2d4cdc3103761903da796049024395a8b7ea31c7
bhawesh2009 <bpaliwal1980@gmai>
2021-06-22 14:16:49 +0530

    bhawesh

---
+++ frontend/build/precache-manifest.6f481c712dce8f0a8995fddae74b0a48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f834cbd910c1d55d74abd6c4316438",
+    "url": "/bhawesh2009/rajsaamndshop.git/index.html"
+  },
+  {
+    "revision": "7659c100d5eccb1fc2e0",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/css/main.2b3f18e2.chunk.css"
+  },
+  {
+    "revision": "58095d6e74022ce67f26",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/js/2.1f9efdae.chunk.js"
+  },
+  {
+    "revision": "1dfa85350b0f723ae190a357399d612d",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/js/2.1f9efdae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7659c100d5eccb1fc2e0",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/js/main.9067ee85.chunk.js"
+  },
+  {
+    "revision": "b2810b70133e6097828a",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/js/runtime-main.1b1f7705.js"
+  },
+  {
+    "revision": "1c9d922f8b45b01a92a251efbca55bcf",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/media/bg.1c9d922f.png"
+  },
+  {
+    "revision": "e2757ba0f02c7e92f88288a2dc1eadcc",
+    "url": "/bhawesh2009/rajsaamndshop.git/static/media/logo2.e2757ba0.webp"
+  }
+]);
